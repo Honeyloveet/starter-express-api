c5beb983dcb872d294b8f5d7ede41c9eb6d139ba
Samuel Kebede <samuelkebede2000@gmail.com>
2023-03-05 21:43:46 +0300

    changed send to json for sending message

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
     console.log("Just got a request!");
-    res.status(204).send('Hi!!');
+    res.status(204).json(["Hi!!!"]);
 })
 app.listen(process.env.PORT || 3000)
