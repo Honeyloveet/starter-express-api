1c2dd7d4f7d050d3d8592e45716fe19243e1ad79
Samuel Kebede <samuelkebede2000@gmail.com>
2023-03-05 21:46:12 +0300

    changed back to send without any message

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
     console.log("Just got a request!");
-    res.status(204).json(["Hi!!!"]);
+    res.status(204).send();
 })
 app.listen(process.env.PORT || 3000)
