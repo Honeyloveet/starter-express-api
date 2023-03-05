cb0caccbf59b91639dc85c83744fa7982c708852
Samuel Kebede <samuelkebede2000@gmail.com>
2023-03-05 21:20:37 +0300

    removed the response message

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!");
-    res.status(204).send("Welcome!!!");
+    res.status(204);
 })
 app.listen(process.env.PORT || 3000)
