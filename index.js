184e4004a4071119bc53641d5c943a2c005bc393
Samuel Kebede <samuelkebede2000@gmail.com>
2023-03-05 21:12:17 +0300

    Changed the status code from 204 to 200

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.status(204).send('Welcome!!!')
+    console.log("Just got a request!");
+    res.status(200).send("Welcome!!!");
 })
 app.listen(process.env.PORT || 3000)
